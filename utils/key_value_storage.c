308c86cab71529bd47098cdb2007e8b5e27dbbd6
Ivan Efremov <welcomebrianemusp@gmail.com>
2025-12-01 17:41:04 +0700

    Add delete in key_value_storage.c, now you can delete in kv what you want.

--- utils/key_value_storage.c
+++ utils/key_value_storage.c
@@ -14,6 +14,7 @@ int get(int fp, char *search_key, int is_write) {
     // Разделяем строку по запятым (находим запятую, сохраняем полученное значение до нее и после)
     // Сравнить с помощью strcmp
 
+    lseek(fp, 0, SEEK_SET);
     int n;
     int line_idx = 0;
     while ((n = read(fp, buf, KV_BUFFER_SIZE)) > 0) {
@@ -50,7 +51,7 @@ int get(int fp, char *search_key, int is_write) {
 
 void put(int fp, char*key, char* value) {
 
-    if ((get(fp, key, 0)) == 0) {
+    if ((get(fp, key, 1)) == 0) {
         printf("key already exists!\n");
         return;
     }
@@ -62,8 +63,80 @@ void put(int fp, char*key, char* value) {
 
 
 
-void delete(int fp, int key, char* value) {
+void delete(int fp, char *search_key, int is_write, char* filename) {
+    lseek(fp, 0, SEEK_SET);
+    char *temp_name = "temp_file.txt";
+    int temp_file = open(temp_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    int n;
+    int line_idx = 0;
+    int found = 0;
+    while ((n = read(fp, buf, KV_BUFFER_SIZE)) > 0) {
+        for (int i = 0; i < n; i++) {
+            if (buf[i] == '\n') {
+                line_buff[line_idx++] = '\0';
+                char *comma_pos = strchr(line_buff, ',');
 
+                if (comma_pos != NULL) {
+                    *comma_pos = '\0';
+
+                    char *file_key = line_buff;
+                    char *file_value = comma_pos + 1;
+
+                    if ((strcmp(file_key, search_key)) == 0) {
+                        line_idx = 0;
+                        found = 1;
+                        continue;
+                    } else {
+                        write(temp_file, file_key, sizeof(char) * strlen(file_key));
+                        write(temp_file, ",", sizeof(char));
+                        write(temp_file, file_value, sizeof(char) * strlen(file_value));
+                        write(temp_file, "\n", sizeof(char));
+                    }
+                }
+                line_idx = 0;
+
+            } else {
+                if (line_idx < sizeof(line_buff) - 1) {
+                    line_buff[line_idx++] = buf[i];
+                }
+            }
+        }
+    }
+
+    if (line_idx > 0) {
+        line_buff[line_idx] = '\0';
+        char *comma_pos = strchr(line_buff, ',');
+        if (comma_pos != NULL) {
+            *comma_pos = '\0';
+            char *file_key = line_buff;
+            char *file_value = comma_pos + 1;
+
+            if ((strcmp(file_key, search_key)) != 0) {
+                write(temp_file, file_key, sizeof(char) * strlen(file_key));
+                write(temp_file, ",", sizeof(char));
+                write(temp_file, file_value, sizeof(char) * strlen(file_value));
+                write(temp_file, "\n", sizeof(char));
+            } else {
+                found = 1;
+            }
+        }
+    }
+
+    close(fp);
+    close(temp_file);
+
+    if (found == 1) {
+        remove(filename);
+        if ((rename(temp_name, filename))) {
+            printf("kv: rename error!\n");
+            exit(1);
+        };
+    } else {
+        remove(temp_name);
+        if (is_write == 0) {
+            printf("Key not found!\n");
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -80,7 +153,7 @@ int main(int argc, char **argv) {
                 get(fp, search_key, 0);
                 close(fp);
             } else {
-                printf("kv: file not found!\n");
+                printf("kv: error with open file!\n");
                 exit(1);
             }
         }
@@ -95,7 +168,18 @@ int main(int argc, char **argv) {
                 printf("kv: Error with open file!");
                 exit(1);
             }
-        } else {
+        }
+        else if (strcmp(argv[1], "delete") == 0) {
+            if ((fp = open(argv[2], O_RDONLY)) != 0) {
+                char *key = argv[3];
+                char *value = argv[4];
+                delete(fp, key, 0, argv[2]);
+            } else {
+                printf("kv: Error with open file!");
+                exit(1);
+            }
+        }
+        else {
             printf("kv: not right arguments\n");
             exit(1);
         }
--- utils/key_value_storage.h
+++ utils/key_value_storage.h
@@ -9,4 +9,11 @@
 static char line_buff[4096];
 static char buf[1024];
 
+// Example of commands:
+// (ofc for linux btw) ./kv {command (get, put or delete)} {file in you will write} {key} {value but not for all commands}
+
+int get(int fp, char *search_key, int is_write);
+void put(int fp, char *key, char *value);
+void delete(int fp, char *search_key, int is_write, char *filename);
+
 #endif //C_PROJECTS_FROM_NEWBIE_KEY_VALUE_STORAGE_H
