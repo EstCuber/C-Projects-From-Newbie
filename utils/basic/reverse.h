b91e49dc52b90b427451fbc0040c42bb1af33044
Ivan Efremov <welcomebrianemusp@gmail.com>
2025-12-02 13:49:11 +0700

    add instruction with flags

--- utils/basic/reverse.h
+++ utils/basic/reverse.h
@@ -10,4 +10,10 @@
 static char buf[REVERSE_BUFF_SIZE];
 static char line_buff[REVERSE_BUFF_SIZE];
 
+// two flags (must have!)
+// 1. -half (reverse only the order of lines)
+// 2. -full (reverse order and lines)
+// for using need to compile this and after that (on linux) use ./reverse hello.txt -half (for example)
+void reverse(int fp, char* _flag);
+
 #endif //C_PROJECTS_FROM_NEWBIE_REVERSE_H
