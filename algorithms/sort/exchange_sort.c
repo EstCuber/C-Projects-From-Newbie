0df6b2bf8d69fa360b7d6ffd895e8b1a43f29a86
Ivan Efremov <welcomebrianemusp@gmail.com>
2025-12-04 18:11:23 +0700

    add exchange_sort.c and exchange_sort.h

--- algorithms/sort/exchange_sort.c
+++ algorithms/sort/exchange_sort.c
@@ -2,20 +2,36 @@
 // Created by ivanefremov on 02.12.2025.
 //
 
+
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "exchange_sort.h"
 
-
-
-typedef struct ArrObj {
-    int *arr;
-    int size;
-} ArrObj;
-
-void exchange_sort(void* arr, size_t size, int* (compare)(const void*, const void*)) {
-
-}
-
-int main() {
-
+void _exchange_sort(
+    void* base,
+    size_t count,
+    size_t size,
+    compare_func_t cmp
+    )
+{
+    char *ptr = (char*) base;
+    void *temp = malloc(size);
+    if (!temp) return;
+    
+    for (size_t i = 0; i < count; ++i) {
+        for (size_t j = i+1; j < count; ++j) {
+            
+            char *a = ptr + (i * size);
+            char *b = ptr + (j * size);
+            
+            if (cmp(a, b) > 0) {
+                memcpy(temp, a, size);
+                memcpy(a, b, size);
+                memcpy(b, temp, size);
+            }
+        }
+    }
+    
+    free(temp);
 }
---
+++ algorithms/sort/exchange_sort.h
@@ -0,0 +1,21 @@
+#ifndef C_PROJECTS_FROM_NEWBIE_EXCHANGE_SORT_H
+#define C_PROJECTS_FROM_NEWBIE_EXCHANGE_SORT_H
+#include <stdio.h>
+
+typedef int (compare_func_t)(const void*, const void*);
+
+void _exchange_sort(
+    void *base, 
+    size_t count, 
+    size_t size, 
+    compare_func_t cmp
+);
+
+#define exchange_sort(base, count, cmp) _exchange_sort(base, count, sizeof(*arr), cmp)
+
+typedef struct ArrObj {
+    int *arr;
+    int size;
+} ArrObj;
+
+#endif
